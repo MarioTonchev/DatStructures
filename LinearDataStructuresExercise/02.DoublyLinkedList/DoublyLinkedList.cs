d184975a59266ab0a43a0091eaa7763277616c2f
MarioT <142032374+MarioTonchev@users.noreply.github.com>
2023-12-03 16:43:59 +0200

    Add files via upload

---
+++ LinearDataStructuresExercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,132 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public T Value { get; set; }
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+
+        }
+
+        private Node head;
+        private Node tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var newNode = new Node(item);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                head.Previous = newNode;
+                newNode.Next = head;
+                head = newNode;
+            }
+            Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node(item);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = tail;
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return head.Value;
+        }
+
+        public T GetLast()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return tail.Value;
+        }
+
+        public T RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var currentHead = head;
+            if (head.Next == null)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                var newHead = head.Next;
+                newHead.Previous = null;
+                head = newHead;
+            }
+            Count--;
+            return currentHead.Value;
+        }
+
+        public T RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var currentTail = tail;
+            if (Count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.Previous;
+                tail.Next = null;
+            }
+            Count--;
+            return currentTail.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = head;
+            while (node != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LinearDataStructuresExercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>(parentheses.Length / 2);
+            
+            foreach (char currentChar in parentheses)
+            {
+                char expectedChar = default;
+                switch (currentChar)
+                {
+                    case ')':
+                        expectedChar = '(';
+                        break;
+                    case ']':
+                        expectedChar = '[';
+                        break;                                  
+                    case '}':
+                        expectedChar = '{';
+                        break;
+                    default:
+                        stack.Push(currentChar);
+                        break;
+                }
+                if (expectedChar == default)
+                {
+                    continue;
+                }
+                if (stack.Pop() != expectedChar)
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+
+
+    }
+}
